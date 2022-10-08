a75d5a0db5e1ec21b78a17ac8bb8972c875c991a
0maru <0maru.dev@gmail.com>
2022-10-09 01:14:47 +0900

    data loader

--- sandbox/go/go-ent-gqlgen-atlas/ent.resolvers.go
+++ sandbox/go/go-ent-gqlgen-atlas/ent.resolvers.go
@@ -21,7 +21,7 @@ func (r *queryResolver) Nodes(ctx context.Context, ids []int) ([]ent.Noder, erro
 
 // Todos is the resolver for the todos field.
 func (r *queryResolver) Todos(ctx context.Context, after *ent.Cursor, first *int, before *ent.Cursor, last *int, orderBy *ent.TodoOrder) (*ent.TodoConnection, error) {
-	return r.client.Todo.Query().
+	return r.client.Debug().Todo.Query().
 		Paginate(ctx, after, first, before, last, ent.WithTodoOrder(orderBy))
 }
 
