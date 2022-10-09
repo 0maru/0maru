b76419db5cd02fb926fcf7631792a676f03f34cc
0maru <0maru.dev@gmail.com>
2022-10-10 01:32:32 +0900

    WIP

--- sandbox/go/go-ent-gqlgen-atlas/cmd/main.go
+++ sandbox/go/go-ent-gqlgen-atlas/cmd/main.go
@@ -1,41 +1,48 @@
 package main
 
 import (
+	"ariga.io/atlas/sql/migrate"
 	"context"
-	"entgo.io/contrib/entgql"
 	"entgo.io/ent/dialect"
-	"entgo.io/ent/entc/integration/migrate/versioned/migrate"
-	todo "github.com/0maru/0maru/sandbox/go/go-ent-gqlgen-atlas"
-	"github.com/0maru/0maru/sandbox/go/go-ent-gqlgen-atlas/ent"
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
+	"entgo.io/ent/dialect/sql"
+	"entgo.io/ent/dialect/sql/schema"
+	"entgo.io/ent/entc"
+	"entgo.io/ent/entc/gen"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
-	"net/http"
+	"os"
 )
 
 func main() {
-	ds := "postgresql://postgres:pass@localhost:5432/test?sslmode=disable"
-	client, err := ent.Open(dialect.Postgres, ds)
+	// We need a name for the new migration file.
+	if len(os.Args) < 2 {
+		log.Fatalln("no name given")
+	}
+	// Create a local migration directory.
+	dir, err := migrate.NewLocalDir("migrations")
 	if err != nil {
-		log.Fatal("opening ent client", err)
+		log.Fatalln(err)
 	}
-	if err := client.Schema.Create(
-		context.Background(),
-		migrate.WithGlobalUniqueID(true),
-	); err != nil {
-		log.Fatal("opening ent client", err)
+	// Load the graph.
+	graph, err := entc.LoadGraph("./ent/schema", &gen.Config{})
+	if err != nil {
+		log.Fatalln(err)
 	}
-
-	// Configure the server and start listening on :8081.
-	srv := handler.NewDefaultServer(todo.NewSchema(client))
-	srv.Use(entgql.Transactioner{TxOpener: client})
-	http.Handle("/",
-		playground.Handler("Todo", "/query"),
-	)
-	http.Handle("/query", srv)
-	log.Println("listening on :8081")
-	if err := http.ListenAndServe(":8081", nil); err != nil {
-		log.Fatal("http server terminated", err)
+	tbls, err := graph.Tables()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Open connection to the database.
+	drv, err := sql.Open(dialect.Postgres, "postgresql://postgres:pass@localhost:5432/test?sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Inspect the current database state and compare it with the graph.
+	m, err := schema.NewMigrate(drv, schema.WithDir(dir))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err := m.NamedDiff(context.Background(), os.Args[1], tbls...); err != nil {
+		log.Fatalln(err)
 	}
 }
