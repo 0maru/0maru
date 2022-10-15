359a3990b3fbf29efca30eb480a7d90ff323912d
0maru <0maru.dev@gmail.com>
2022-10-15 23:38:53 +0900

    スキーマを定義

--- sandbox/go/graphql-sample/ent/schema/todo.go
+++ sandbox/go/graphql-sample/ent/schema/todo.go
@@ -1,6 +1,12 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+	"time"
+)
 
 // Todo holds the schema definition for the Todo entity.
 type Todo struct {
@@ -9,7 +15,21 @@ type Todo struct {
 
 // Fields of the Todo.
 func (Todo) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(uuid.New()).
+			Unique(),
+		field.Text("description"),
+		field.Bool("completed").
+			Default(false),
+		field.Time("created_at").
+			Default(time.Now).
+			Annotations(&entsql.Annotation{
+				Default: "CURRENT_TIMESTAMP",
+			}),
+		field.Time("updated_at").
+			Default(time.Now),
+	}
 }
 
 // Edges of the Todo.
