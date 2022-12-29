1ec2e789a4301a72a3ceca700af4bb166b028d7a
0maru <0maru.dev@gmail.com>
2022-12-29 23:49:05 +0900

    graphql のスキーマはGithub のものを使う

--- sandbox/nextjs/nextjs-v13-relay/relay.config.js
+++ sandbox/nextjs/nextjs-v13-relay/relay.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     src: "./src",
     language: "typescript",
-    schema: "./schema.graphql",
+    schema: "./schema.docs.graphql",
     exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
 }
