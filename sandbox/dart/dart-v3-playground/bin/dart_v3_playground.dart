ce5fcca67b23eeaec65da09d87482f649c57b21f
0maru <0maru.dev@gmail.com>
2023-01-04 23:01:03 +0900

    create dart v3 project

---
+++ sandbox/dart/dart-v3-playground/bin/dart_v3_playground.dart
@@ -0,0 +1,5 @@
+import 'package:dart_v3_playground/dart_v3_playground.dart' as dart_v3_playground;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_v3_playground.calculate()}!');
+}
---
+++ sandbox/dart/dart-v3-playground/test/dart_v3_playground_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_v3_playground/dart_v3_playground.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
